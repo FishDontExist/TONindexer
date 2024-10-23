5b62cb218f4548e92298e0deb98816cb931976fc
fishdontexist <sdrhashemi@gmail.com>
2024-10-23 22:08:00 +0330

    Fix some debugging

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/FishDontExist/TONindexer/api"
+import (
+	"fmt"
+
+	"github.com/FishDontExist/TONindexer/chain"
+)
 
 func main() {
+	pk := []string{"essay", "route", "raise", "title", "field", "dumb", "torch", "desert", "vocal", "seminar", "sketch", "soda", "burger", "daughter", "clog", "cup", "best", "helmet", "another", "federal", "cause", "long", "bullet", "grape"}
+	ln := chain.New()
+	// result, _ := ln.GetTransactions("UQB03G_qlolqO67Q5nutMkJ4Yy84o8r5_b_Ijam8Lfj0t1sC")
+	_, ok := ln.Transfer("UQB03G_qlolqO67Q5nutMkJ4Yy84o8r5_b_Ijam8Lfj0t1sC", pk, 0.03)
+	if ok {
+		fmt.Println("done")
+	} else{
+		fmt.Println("failed")
+	}
 
-	api.SetApi()
+	
+
 }
