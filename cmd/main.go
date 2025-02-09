3e7b439fdd4c3f988f8d732a3fb062e79b079180
Sadra Hashemi <107583285+FishDontExist@users.noreply.github.com>
2025-02-09 14:55:08 +0330

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,7 @@ package main
 import "github.com/FishDontExist/TONindexer/api"
 
 func main() {
-	// pk := []string{"essay", "route", "raise", "title", "field", "dumb", "torch", "desert", "vocal", "seminar", "sketch", "soda", "burger", "daughter", "clog", "cup", "best", "helmet", "another", "federal", "cause", "long", "bullet", "grape"}
-
-	// ln.SendJetton(pk, "0.04", "UQB03G_qlolqO67Q5nutMkJ4Yy84o8r5_b_Ijam8Lfj0t1sC")
+	
 	api.SetApi()
 
 }
